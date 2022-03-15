ef73e43476303e48e2fd4060505cf60f3e1e4868
jbueno <jbueno>
2022-03-15 10:56:11 +0100

    [spain_model]
    Air network

---
+++ src/main/java/core/graph/air/AirLink.java
@@ -0,0 +1,86 @@
+package core.graph.air;
+
+import java.time.LocalTime;
+
+import com.opencsv.bean.CsvBindByName;
+import com.opencsv.bean.CsvBindByPosition;
+
+import core.graph.LinkI;
+import core.graph.annotations.GraphElementAnnotation.Neo4JLinkElement;
+import core.graph.annotations.GraphElementAnnotation.Neo4JPropertyElement;
+import core.graph.annotations.GraphElementAnnotation.Neo4JType;
+
+@Neo4JLinkElement(label="AirLink")
+public class AirLink implements LinkI{
+	
+	@CsvBindByName(column = "stop_from")
+	@CsvBindByPosition(position = 0)
+	@Neo4JPropertyElement(key="from",type=Neo4JType.TOSTRING)
+	private String from;
+	@CsvBindByName(column = "stop_to")
+	@CsvBindByPosition(position = 1)
+	@Neo4JPropertyElement(key="to",type=Neo4JType.TOSTRING)
+	private String to;
+	@CsvBindByName(column = "avg_travel_time")
+	@CsvBindByPosition(position = 2)
+	@Neo4JPropertyElement(key="avg_travel_time",type=Neo4JType.TOFLOAT)
+	private Double avgTravelTime;
+	@CsvBindByName(column = "connections_per_day")
+	@CsvBindByPosition(position = 3)
+	@Neo4JPropertyElement(key="connections_per_day",type=Neo4JType.TOINTEGER)
+	private Integer connectionsPerDay;
+	@CsvBindByName(column = "first_dep_time")
+	@CsvBindByPosition(position = 4)
+	@Neo4JPropertyElement(key="first_dep_time",type=Neo4JType.TOSTRING)
+	private LocalTime firstDepartureTime;
+	@CsvBindByName(column = "last_dep_time")
+	@CsvBindByPosition(position = 5)
+	@Neo4JPropertyElement(key="last_dep_time",type=Neo4JType.TOSTRING)
+	private LocalTime lastDepartureTime;
+	@CsvBindByName(column = "type")
+	@CsvBindByPosition(position = 6)
+	private String type;
+	
+	public AirLink() {}
+	
+	public AirLink( String from,String to,Double avgTravelTime,
+			Integer connectionsPerDay,LocalTime firstDepartureTime,
+			LocalTime lastDepartureTime,String type) {
+		this.from = from;
+		this.to = to;
+		this.avgTravelTime = avgTravelTime;
+		this.connectionsPerDay = connectionsPerDay;
+		this.firstDepartureTime = firstDepartureTime;
+		this.lastDepartureTime = lastDepartureTime;
+		this.type = type;
+	}
+	
+	public String getFrom() {
+		return this.from;
+	}
+	
+	public String getTo(){
+		return this.to; 
+	}
+	
+	public Double getAvgTravelTime(){
+		return this.avgTravelTime; 
+	}
+	
+	public Integer getConnectionsPerDay(){
+		return this.connectionsPerDay; 
+	}
+	
+	public LocalTime getFirstDepartureTime(){
+		return this.firstDepartureTime; 
+	}
+	
+	public LocalTime getLastDepartureTime(){
+		return this.lastDepartureTime; 
+	}
+	
+	public String getType(){
+		return this.type; 
+	}
+
+}
---
+++ src/main/java/core/graph/air/gtfs/StopTime.java
@@ -0,0 +1,65 @@
+package core.graph.air.gtfs;
+
+import java.time.LocalTime;
+import java.util.Date;
+
+import com.opencsv.bean.AbstractBeanField;
+import com.opencsv.bean.CsvBindByPosition;
+import com.opencsv.bean.CsvCustomBindByPosition;
+import com.opencsv.bean.CsvDate;
+import com.opencsv.exceptions.CsvConstraintViolationException;
+import com.opencsv.exceptions.CsvDataTypeMismatchException;
+
+public final class StopTime {
+	
+	@CsvBindByPosition(position = 0)
+	private String tripId;
+	
+	@CsvCustomBindByPosition(position = 1, converter = LocalTimeConverterGTFS.class)
+	private LocalTime arrivalTime;
+	
+	@CsvCustomBindByPosition(position = 2, converter = LocalTimeConverterGTFS.class)
+	private LocalTime departureTime;
+	@CsvBindByPosition(position = 3)
+	private String stopId;
+	@CsvBindByPosition(position = 4)
+	private Byte stopSequence;
+	@CsvBindByPosition(position = 5)
+	private Boolean stopHeadsign;
+	@CsvBindByPosition(position = 6)
+	private Boolean pickupType;
+	@CsvBindByPosition(position = 7)
+	private Boolean dropOffType;
+	@CsvBindByPosition(position = 8)
+	private Integer shapeDistTraveled;
+	
+	
+	public String getTripId() {
+		return this.tripId;
+	}
+	public LocalTime getArrivalTime() {
+		return this.arrivalTime;
+	}
+	public LocalTime getDepartureTime() {
+		return this.departureTime;
+	}
+	public String getStopId() {
+		return this.stopId;
+	}
+	public Byte getStopSequence() {
+		return this.stopSequence;
+	}
+	public Boolean getStopHeadsign() {
+		return this.stopHeadsign;
+	}
+	public Boolean getPickupType() {
+		return this.pickupType;
+	}
+	public Boolean getDropOffType() {
+		return this.dropOffType;
+	}
+	public Integer getShapeDistTraveled() {
+		return this.shapeDistTraveled;
+	}
+
+}
